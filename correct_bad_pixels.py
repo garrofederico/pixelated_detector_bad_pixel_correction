c8f6ec07eab4dd251b313810704c0f6e13bf9010
Federico Garro <garrofederico@gmail.com>
2022-08-30 13:50:28 +0200

    Replaced .yaml config files with ArgParser lib

--- correct_bad_pixels.py
+++ correct_bad_pixels.py
@@ -10,23 +10,38 @@ import os
 import yaml
 from yacs.config import CfgNode as CN
 import datetime
+import os
+import sys
+import argparse
+from pathlib import Path
 
+# Create the parser
+my_parser = argparse.ArgumentParser(description='Corrects the bad pixels of a pixem 4D dataset and outputs a .zspy file')
 
-# config_file = 'configs/config_federico.yml'
-config_file = './configs/config_gulnaz_new_dataset.yml'
-NEEDS_RESHAPING = True
-os.chdir('.')
-print(os.getcwd())
+# Add the arguments
+my_parser.add_argument('-p', '--paths', metavar='path/to/dataset(s)', nargs='+', type=str, required=True,
+                       help='the path(s) to the 4D dataset (can be multiple paths separated by space)')
+my_parser.add_argument('-d', '--dimensions', metavar='dimension', nargs=2, type=int, required=True,
+                       help='dimensions of the dataset (without including the flyback pixel) e.g. 640 640')
 
-# Load config file
-with open(config_file, "r") as stream:
-    try:
-        cfg = yaml.safe_load(stream)
+# Execute the parse_args() method
+args = my_parser.parse_args()
 
-    except yaml.YAMLError as exc:
-        print(exc)
-cfg = CN(cfg)
-print(cfg)
+input_paths = [Path(x) for x in args.paths]
+input_dims = args.dimensions
+
+for i, path in enumerate(input_paths):
+    print(f"the path {i + 1} is {path}")
+
+    if not path.exists():
+        print('The path specified does not exist')
+        sys.exit()
+
+    print(input_dims)
+reshaped_path = path.with_name(path.name + '_reshaped.hspy')
+converted_path = reshaped_path.with_suffix('.zspy')
+corrected_path = converted_path.with_name(converted_path.stem+ '_corrected.zspy')
+
 
 
 
